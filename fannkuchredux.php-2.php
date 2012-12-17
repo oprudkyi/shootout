6af4d9efa0a3227bf89fa863eac7bd17b15cbc7e
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2012-12-17 19:17:15 +0200

    ~20%-30% faster comparing with current version http://shootout.alioth.debian.org/u32/program.php?test=fannkuchredux&lang=php&id=1 (17,717s vs 23,339sec for N=10 at my PC)
    Changes:
    - deep optimization of code

--- fannkuchredux.php-2.php
+++ fannkuchredux.php-2.php
@@ -1,53 +1,67 @@
-<? /* The Computer Language Benchmarks Game
+<?php 
+/* The Computer Language Benchmarks Game
    http://shootout.alioth.debian.org/
    
    contributed by Isaac Gouy, transliterated from Mike Pall's Lua program 
+   further optimization by Oleksii Prudkyi
 */
 
-function Fannkuch($n){
-   $p = $q = $s = array(); 
-   $sign = 1; $maxflips = $sum = 0; $m = $n-1;
-   for ($i=0; $i<$n; $i++){ $p[$i] = $i; $q[$i] = $i; $s[$i] = $i; }      
-   do {
-      // Copy and flip.
-      $q0 = $p[0];                                          // Cache 0th element.
-      if ($q0 != 0){
-         for($i=1; $i<$n; $i++) $q[$i] = $p[$i];            // Work on a copy.
-         $flips = 1;
-         do { 
-            $qq = $q[$q0]; 
-            if ($qq == 0){                                  // ... until 0th element is 0.
-               $sum += $sign*$flips;
-	       if ($flips > $maxflips) $maxflips = $flips;  // New maximum?
-               break; 
-            } 
- 	    $q[$q0] = $q0; 
-	    if ($q0 >= 3){
-	       $i = 1; $j = $q0 - 1;
-               do { $t = $q[$i]; $q[$i] = $q[$j]; $q[$j] = $t; $i++; $j--; } while ($i < $j); 
-            }
-	    $q0 = $qq; $flips++;
-         } while (true); 
-      }
-      // Permute.
-      if ($sign == 1){
-         $t = $p[1]; $p[1] = $p[0]; $p[0] = $t; $sign = -1; // Rotate 0<-1.
-      } else { 
-         $t = $p[1]; $p[1] = $p[2]; $p[2] = $t; $sign = 1;  // Rotate 0<-1 and 0<-1<-2.
-         for($i=2; $i<$n; $i++){ 
-	    $sx = $s[$i];
-	    if ($sx != 0){ $s[$i] = $sx-1; break; }
-	    if ($i == $m) return array($sum,$maxflips);     // Out of permutations.
-	    $s[$i] = $i;
-	    // Rotate 0<-...<-i+1.
-	    $t = $p[0]; for($j=0; $j<=$i; $j++){ $p[$j] = $p[$j+1]; } $p[$i+1] = $t;
-         }
-      }
-   } while (true);
-}
-
-$n = $argv[1];
-list($checksum,$pf) = Fannkuch($n);
-printf("%d\nPfannkuchen(%d) = %d", $checksum, $n, $pf);
-?>
-
+$n = (int)$argv[1];
+$s = range(0, $n - 1);
+$i = $maxflips = $checksum = 0; 
+$sign = 1; 
+$m = $n - 1;
+$p = $q = $s;
+do {
+	// Copy and flip.
+	$q0 = $p[0];
+	if ($q0 != 0){
+		$q = $p;
+		$flips = 1;
+		do { 
+			$qq = $q[$q0]; 
+			if ($qq == 0){
+				$checksum += $sign*$flips;
+				if ($flips > $maxflips) $maxflips = $flips;
+				break; 
+			} 
+			$q[$q0] = $q0; 
+			if ($q0 >= 3){
+				$i = 1; $j = $q0 - 1;
+				do { 
+					$t = $q[$i]; 
+					$q[$i] = $q[$j]; 
+					$q[$j] = $t; 
+					++$i;
+					--$j;
+				} while ($i < $j); 
+			}
+			$q0 = $qq; 
+			++$flips;
+		} while (true); 
+	}
+	// Permute.
+	if ($sign == 1){
+		$t = $p[1]; $p[1] = $p[0]; $p[0] = $t; $sign = -1; // Rotate 0<-1.
+	} else { 
+		$t = $p[1]; $p[1] = $p[2]; $p[2] = $t; $sign = 1;  // Rotate 1<-2.
+		for($i=2; $i<$n; ){ 
+			$sx = &$s[$i];
+			if ($sx != 0)
+			{
+				--$sx; 
+				break; 
+			}
+			if ($i == $m){
+				printf("%d\nPfannkuchen(%d) = %d\n", $checksum, $n, $maxflips);// Out of permutations.
+				return;
+			}
+			$s[$i] = $i;
+			// Rotate 0<-...<-i+1.
+			$t = $p[0]; 
+			for($j=0; $j<=$i; ){ $p[$j++] = $p[$j]; } 
+			++$i;
+			$p[$i] = $t;
+		}
+	}
+} while (true);
